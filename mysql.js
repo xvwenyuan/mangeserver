ac397adb0211f5dc8710ec304cc26baf9bf72592
yyqq <704984147@qq.com>
2020-04-30 22:40:56 +0800

    manageserver

---
+++ mysql.js
@@ -0,0 +1,153 @@
+﻿const mysql = require('mysql');
+var pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'root',
+    database: 'bbs'
+});
+let allServices = {
+    query: function (sql, values) {
+
+        return new Promise((resolve, reject) => {
+            pool.getConnection(function (err, connection) {
+                if (err) {
+                    reject(err)
+                } else {
+                    connection.query(sql, values, (err, rows) => {
+
+                        if (err) {
+                            reject(err)
+                        } else {
+                            resolve(rows)
+                        }
+                        connection.release()
+                    })
+                }
+            })
+        })
+
+    },
+    findUserData: function (page,tableName) {//商品分页查询
+        // let _sql = `select * from user where username="${name}";`
+        let _sql = `select * from ${tableName} limit ${page},5`
+        return allServices.query(_sql)
+    },
+    check: (obj) => {//校验管理员用户名和密码
+        let _sql = `select * from admin where username = '${obj.username}' and password = '${obj.password}'`
+        return allServices.query(_sql, obj)
+    },
+    groupSetting: (obj) => {//开团设置
+        console.log(obj.person, obj.time)
+        let _sql = `update groupbuyset set person=${obj.person},time=${obj.time} where id=1`
+        console.log(_sql)
+        return allServices.query(_sql, obj)
+    },
+    addGoods: (obj) => {//插入普通商品
+        let _sql = "insert into goods set goods_id=?,goods_desc=?,goods_price=?,goods_url=?,goods_detailurl=?,goods_name=?,issale=1;"
+        console.log(_sql)
+        return allServices.query(_sql, obj)
+    },
+    addGroupGoods: (obj) => {//插入团购商品
+        let _sql = "insert into groupgoods set groupgoods_id=?,groupgoods_desc=?,groupgoods_originalprice=?,groupgoods_groupbuyprice=?,groupgoods_url=?,groupgoods_detailurl=?,groupgoods_name=?,groupgoods_sale=?,issale=1;"
+        return allServices.query(_sql, obj)
+    },
+    sale: (obj) => { //普通商品上下架
+        let _sql = `update goods set issale=${obj.issale} where goods_id='${obj.goods_id}'`
+        return allServices.query(_sql, obj)
+    },
+    select: (obj) => { //普通商品模糊查询全部
+        if (obj.content) {
+            if (obj.state !== 2) {
+                let _sql = `select * from goods where goods_name like '%${obj.content}%' and issale=${obj.state};`
+                return allServices.query(_sql, obj)
+            } else {
+                let _sql = `select * from goods where goods_name like '%${obj.content}%';`
+                return allServices.query(_sql, obj)
+            }
+        } else {
+            if (obj.state !== 2) {
+                let _sql = `select * from goods where issale=${obj.state};`
+                return allServices.query(_sql, obj)
+            } else {
+                let _sql = 'select * from goods;'
+                return allServices.query(_sql, obj)
+            }
+        }
+    },
+    updateGoods: (obj) => { //普通商品编辑
+        let _sql = `update goods set goods_desc='${obj.goods_desc}',goods_price='${obj.goods_price}',goods_url='${obj.goods_url}',goods_detailurl='${obj.goods_detailurl}',goods_name='${obj.goods_name}' where goods_id=${obj.goods_id}`
+        return allServices.query(_sql, obj)
+    },
+    selectGroup: (obj) => { //团购商品模糊查询
+        if (obj.content) {
+            if (obj.state !== 2) {
+                let _sql = `select * from groupgoods where groupgoods_name like '%${obj.content}%' and issale=${obj.state};`
+                return allServices.query(_sql, obj)
+            } else {
+                let _sql = `select * from groupgoods where groupgoods_name like '%${obj.content}%';`
+                return allServices.query(_sql, obj)
+            }
+        } else {
+            if (obj.state !== 2) {
+                let _sql = `select * from groupgoods where issale=${obj.state};`
+                return allServices.query(_sql, obj)
+            } else {
+                let _sql = 'select * from groupgoods;'
+                return allServices.query(_sql, obj)
+            }
+        }
+    },
+    saleGroup: (obj) => { //团购商品上下架
+        let _sql = `update groupgoods set issale=${obj.issale} where groupgoods_id='${obj.goods_id}'`
+        return allServices.query(_sql, obj)
+    },
+    updateGruoupGoods: (obj) => { //团购商品商品编辑
+        let _sql = `update groupgoods set groupgoods_desc='${obj.groupgoods_desc}',groupgoods_originalprice='${obj.groupgoods_originalprice}',groupgoods_groupbuyprice='${obj.groupgoods_groupbuyprice}',groupgoods_url='${obj.groupgoods_url}',groupgoods_detailurl='${obj.groupgoods_detailurl}',groupgoods_name='${obj.groupgoods_name}',groupgoods_sale='${obj.groupgoods_sale}' where groupgoods_id='${obj.groupgoods_id}';`
+        return allServices.query(_sql, obj)
+    },
+    getAll: function (tableName) {//查询全部商品
+        // let _sql = `select * from user where username="${name}";`
+        let _sql = `select * from ${tableName};`
+        return allServices.query(_sql)
+    },
+    selectPage:(obj) => { //普通商品模糊查询
+        if (obj.content) {
+            if (obj.state !== 2) {
+                let _sql = `select * from goods where goods_name like '%${obj.content}%' and issale=${obj.state} limit ${obj.page},5;`
+                return allServices.query(_sql, obj)
+            } else {
+                let _sql = `select * from goods where goods_name like '%${obj.content}%' limit ${obj.page},5;`
+                return allServices.query(_sql, obj)
+            }
+        } else {
+            if (obj.state !== 2) {
+                let _sql = `select * from goods where issale=${obj.state} limit ${obj.page},5;`
+                return allServices.query(_sql, obj)
+            } else {
+                let _sql = `select * from goods limit ${obj.page},5;`
+                return allServices.query(_sql, obj)
+            }
+        }
+    },
+    selectGroupPage: (obj) => { //团购商品模糊查询分页
+        if (obj.content) {
+            if (obj.state !== 2) {
+                let _sql = `select * from groupgoods where groupgoods_name like '%${obj.content}%' and issale=${obj.state} limit ${obj.page},5;`
+                return allServices.query(_sql, obj)
+            } else {
+                let _sql = `select * from groupgoods where groupgoods_name like '%${obj.content}%' limit ${obj.page},5;`
+                return allServices.query(_sql, obj)
+            }
+        } else {
+            if (obj.state !== 2) {
+                let _sql = `select * from groupgoods where issale=${obj.state} limit ${obj.page},5;`
+                return allServices.query(_sql, obj)
+            } else {
+                let _sql = `select * from groupgoods limit ${obj.page},5;`
+                return allServices.query(_sql, obj)
+            }
+        }
+    }
+}
+
+module.exports = allServices;
---
+++ server.js
@@ -0,0 +1,209 @@
+﻿const Koa = require('koa');//导入一个class
+// 注意require('koa-router')返回的是函数:
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+const { findUserData, check, groupSetting, addGoods, addGroupGoods, sale, select, updateGoods, selectGroup, saleGroup, updateGruoupGoods, getAll, selectPage, selectGroupPage } = require('./mysql');
+const jwt = require('jsonwebtoken');//生成token
+const md5 = require('js-md5')
+const koajwt = require('koa-jwt')
+var cors = require('koa2-cors');
+const app = new Koa();//创建一个koa对象
+app.use(bodyParser());
+app.use(cors({
+    origin: function (ctx) {
+        return '*';
+    },
+    maxAge: 5,
+    credentials: true,
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+
+}));
+// 错误处理
+app.use((ctx, next) => {
+    return next().catch((err) => {
+        if (err.status === 401) {
+            ctx.status = 401;
+            ctx.body = 'Protected resource, use Authorization header to get access\n';
+        } else {
+            throw err;
+        }
+    })
+})
+
+app.use(koajwt({
+    secret: 'my_token'
+}).unless({
+    path: '/pc/login'
+}));
+// log request URL:
+// 对于任何请求，app将调用该异步函数处理请求
+app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+    await next();
+});
+
+// router.get('/groupgoods', async (ctx, next) => {//获取团购商品
+//     try {
+//         let goodsList = await findUserData('groupgoods');
+//         ctx.response.body = goodsList;
+//     } catch (error) {
+//         console.log('error')
+//     }
+// });
+
+//后台管理系统接口
+router.post('/pc/login', async (ctx, next) => {//后台管理系统登录
+    const username = ctx.request.body.username
+    const password = md5(ctx.request.body.password + '666')//md5加密加盐
+    const obj = { username, password }
+    try {
+        let res = await check(obj)
+        res = JSON.parse(JSON.stringify(res))
+        if (res.length) {//用户名,密码正确，获取token
+            const token = jwt.sign({
+                username,
+                password
+            }, 'my_token', {
+                expiresIn: '4h' //到期时间
+            });
+            ctx.response.body = {
+                username: res[0].username,
+                root: res[0].root,
+                token: token,
+                code: 1
+            }
+        } else {
+            ctx.response.body = '用户名或密码错误！'
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+router.post('/pc/goodlist', async (ctx, next) => {//获取普通商品列表
+    let page = ctx.request.body.page
+    try {
+        let goodsList = await findUserData(page, 'goods');
+        let total = (await getAll('goods')).length;
+        ctx.response.body = { goodsList, total };
+    } catch (error) {
+        console.log('error')
+    }
+})
+router.post('/pc/groupgoodslist', async (ctx, next) => {//获取团购商品列表
+    let page = ctx.request.body.page
+    try {
+        let total = (await getAll('groupgoods')).length;//获取总条目数数
+        let goodsList = await findUserData(page, 'groupgoods');//查询
+        ctx.response.body = { goodsList, total };
+    } catch (error) {
+        console.log('error')
+    }
+})
+router.post('/pc/groupsetting', async (ctx, next) => {//团购设置
+    let setData = ctx.request.body
+    try {
+        await groupSetting(setData)
+        ctx.response.body = '';
+
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+router.post('/pc/addgoods', async (ctx, next) => {//普通商品添加
+    let data = ctx.request.body
+    try {
+        await addGoods([data.goods_id, data.goods_desc, data.goods_price, data.goods_url, data.goods_detailurl, data.goods_name])
+        ctx.response.body = '';
+
+    } catch (error) {
+        console.log('错误')
+    }
+
+})
+router.post('/pc/addgroupgoods', async (ctx, next) => {//团购商品添加
+    let data = ctx.request.body
+    let arr = [data.groupgoods_id, data.groupgoods_desc, data.groupgoods_originalprice, data.groupgoods_groupbuyprice, data.groupgoods_url, data.groupgoods_detailurl, data.groupgoods_name, data.groupgoods_sale]
+    console.log(arr)
+    try {
+        await addGroupGoods(arr);
+        ctx.response.body = '';
+
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+router.post('/pc/editgoods', async (ctx, next) => {//普通商品编辑
+    let setData = ctx.request.body
+    console.log(setData)
+    try {
+        await updateGoods(setData)
+        ctx.response.body = '';
+    } catch (error) {
+        console.log(error)
+    }
+})
+router.post('/pc/editgroupgoods', async (ctx, next) => {//团购商品编辑
+    let setData = ctx.request.body
+    try {
+        await updateGruoupGoods(setData)
+        ctx.response.body = '';
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+router.post('/pc/issale', async (ctx, next) => {//普通商品上下架
+    let setData = ctx.request.body
+    try {
+        sale(setData)
+        ctx.response.body = '';
+    } catch (error) {
+    }
+})
+router.post('/pc/select', async (ctx, next) => {//普通商品模糊查询
+    let setData = ctx.request.body
+    console.log(setData)
+    try {
+        let total = (await select(setData)).length
+        let result = await selectPage(setData)
+        ctx.response.body = { result, total };
+    } catch (error) {
+        console.log(error)
+    }
+})
+router.post('/pc/selectgroup', async (ctx, next) => {//团购商品模糊查询
+    let setData = ctx.request.body
+    console.log(setData)
+    try {
+        let total = (await selectGroup(setData)).length
+        let goodsList = await (selectGroupPage(setData))
+        ctx.response.body = {goodsList,total};
+    } catch (error) {
+        console.log(error)
+    }
+})
+router.post('/pc/issalegroup', async (ctx, next) => {//普通商品上下架
+    let setData = ctx.request.body
+    try {
+        await saleGroup(setData)
+        ctx.response.body = '';
+    } catch (error) {
+        console.log(error)
+    }
+})
+// router.post('/pc/tohome', async (ctx, next) => {//二次登录
+//     const token = ctx.request.body.token
+//     console.log(token)
+//     ctx.response.body = '';
+
+// })
+
+
+// add router middleware:
+app.use(router.routes());
+
+app.listen(3001, '192.168.0.104');//3000端口监听
+console.log('app started at port 3001...');
